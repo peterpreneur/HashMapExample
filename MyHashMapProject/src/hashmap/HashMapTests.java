7713a7d5a5206f7f5eaf532f9611c8f6d3c0d725
pas8 <peterpreneur@gmail.com>
2022-01-22 14:49:09 -0600

    add jvunit

--- MyHashMapProject/src/hashmap/HashMapTests.java
+++ MyHashMapProject/src/hashmap/HashMapTests.java
@@ -1,7 +1,11 @@
 package hashmap;
 
-public class HashMapTests {
-	
-	
+import org.junit.Test;
 
+public class HashMapTests {
+	@Test
+	public void should_insert_value_into_hashmap_and_retrieve_it()
+	{
+		
+	}
 }
